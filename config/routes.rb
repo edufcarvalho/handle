42acddbf1f4f92ba25e7cd60edc65333921ca5b1
Eduardo Freitas de Carvalho <eduardofc@ic.ufrj.br>
2026-04-19 00:33:29 -0300

    fix: cant make routes multi-file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,26 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  extend UserRoutes
+  get "sign_in", to: "sessions#new", as: :sign_in
+  post "sign_in", to: "sessions#create"
+  get  "sign_up", to: "users#new", as: :sign_up
+  post "sign_up", to: "users#create"
+
+  resources :sessions, only: [:destroy]
+  resource :users, only: [:destroy]
+
+  namespace :identity do
+    resource :email_verification, only: [:show, :create]
+    resource :password_reset,     only: [:new, :edit, :create, :update]
+  end
+
+  namespace :settings do
+    resource :profile, only: [:show, :update]
+    resource :password, only: [:show, :update]
+    resource :email, only: [:show, :update]
+    resources :sessions, only: [:index]
+    inertia :appearance
+  end
 
   root "home#index"
 
